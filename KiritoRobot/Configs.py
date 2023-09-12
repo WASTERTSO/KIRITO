e8b24b60d0a82ecf6899f1b03d572242198eb4f7
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-09-12 11:50:57 +0530

    Update Configs.py

--- KiritoRobot/Configs.py
+++ KiritoRobot/Configs.py
@@ -5,7 +5,7 @@ class Config(object):
     APP_ID = os.environ.get("APP_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     TOKEN = os.environ.get("TOKEN", None)
-    BOT_US = os.environ.get("BOT_US", "KiritoXProBot")
+    BOT_US = os.environ.get("BOT_US", "goku_godxbot")
     WELCOME_TEXT = os.environ.get(
         "WELCOME_TEXT", "Cardinal System Is Damaged!, Sorry I Cant Remember You."
     )
